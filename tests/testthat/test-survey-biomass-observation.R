1a40f323cf8874476fb7c9a5b9bedc656db0731e
Craig Marsh <paisaje.partnership@gmail.com>
2023-01-30 17:13:27 -0900

    UPD: nuisance q documentation

--- tests/testthat/test-survey-biomass-observation.R
+++ tests/testthat/test-survey-biomass-observation.R
@@ -85,4 +85,85 @@ test_that("survey-abundance-test", {
 
 })
 
+#'
+#' abundance-test-production
+#'
+test_that("survey-abundance-production-test", {
+  ## Read in mock data
+  load(system.file("testdata", "MockSablefishModel.RData",package="SpatialSablefishAssessment"))
+  data$model = "TagIntegrated"
+  ## no Z or movement
+  data$apply_Z_on_tagged_fish = 0
+  data$apply_fixed_movement = 1
+  data$srv_dom_ll_bio_indicator[data$srv_dom_ll_bio_indicator == 0] = 1
+  data$srv_dom_ll_obs_is_abundance = 1
+  mean_obs_val = 12
+  data$obs_srv_dom_ll_bio = array(mean_obs_val, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  data$obs_srv_dom_ll_se = array(0.3 * mean_obs_val, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  data$srv_dom_ll_bio_comp_likelihood = 0
+  data$q_is_nuisance = 0
 
+  ## make sure it doesn't crash
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+  test_report = test_model$report()
+
+  expect_true(all(test_report$pred_srv_dom_ll_bio > 0))
+
+  ## change likelihood and thus SE intepretation
+  data$obs_srv_dom_ll_se = array(0.3, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  data$srv_dom_ll_bio_comp_likelihood = 1
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+
+  test_report = test_model$report()
+  ## test against R's dlnorm function they should be the same
+  expect_equal(test_report$nll[5], -1.0 * sum(dlnorm(test_report$obs_srv_dom_ll_bio, log(test_report$pred_srv_dom_ll_bio) - 0.5*test_report$obs_srv_dom_ll_se^2, test_report$obs_srv_dom_ll_se, log = T)), tolerance = 0.001)
+
+  ## nuisance q calculation
+  data$q_is_nuisance = 1
+  ## check it doesn't crash
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+  test_report = test_model$report()
+
+
+  ## repeat but for weight instead of abundance
+  data$srv_dom_ll_obs_is_abundance = 0
+  data$srv_dom_ll_bio_comp_likelihood = 0
+  data$q_is_nuisance = 0
+  mean_obs_val = 45
+  data$obs_srv_dom_ll_bio = array(mean_obs_val, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  data$obs_srv_dom_ll_se = array(0.3 * mean_obs_val, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  ## check it doesn't crash
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+  test_report = test_model$report()
+  test_report = test_model$report()
+
+  expect_true(all(test_report$pred_srv_dom_ll_bio > 0))
+
+  ## change likelihood and thus SE intepretation
+  data$obs_srv_dom_ll_se = array(0.3, dim = c(data$n_regions, ncol(data$srv_dom_ll_bio_indicator)))
+  data$srv_dom_ll_bio_comp_likelihood = 1
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+
+  test_report = test_model$report()
+  ## test against R's dlnorm function they should be the same
+  expect_equal(test_report$nll[5], -1.0 * sum(dlnorm(test_report$obs_srv_dom_ll_bio, log(test_report$pred_srv_dom_ll_bio) - 0.5*test_report$obs_srv_dom_ll_se^2, test_report$obs_srv_dom_ll_se, log = T)), tolerance = 0.001)
+
+  ## nuisance q calculation
+  data$q_is_nuisance = 1
+  ## check it doesn't crash
+  expect_no_condition(test_model <- TMB::MakeADFun(data = data,
+                                                   parameters = parameters,
+                                                   DLL = "SpatialSablefishAssessment_TMBExports", silent  = T))
+  test_report = test_model$report()
+
+})
