40d6486c1f821dc84668e20a3abdfc359cda3ba6
Craig Marsh <paisaje.partnership@gmail.com>
2023-09-07 11:36:42 +0100

    ADD: how to use MCMC functionality in Gitbook
    ADD; new selectivity to show during presentation

--- inst/include/SetupSelectivities.hpp
+++ inst/include/SetupSelectivities.hpp
@@ -41,6 +41,42 @@ vector<Type> double_normal_ogive(vector<Type>& ages, Type& sel_50, Type& delta) 
   return d_norm;
 }
 
+// Taken from the SS3 manual
+template <class Type>
+vector<Type> double_normal_SS3_ogive(vector<Type>& ages, Type& min_x_val_, Type& first_sel_val_, Type& p1trans_, Type& p2trans_, Type& p3trans_, Type& p4trans_, Type& p5trans_, Type& p6trans_) {
+  //std::cout << "logistic_ogive\n";
+  int n_ages = ages.size();
+  vector<Type> d_norm(n_ages);
+  // NOTE: NEED to get the model min and max age or min and max size, and the bin width (assumed to be 1 below)
+  // min age is popn.state.col_min or get_int("min_age")
+  // max age is popn.state.col_max or get_int("max_age")
+  // length bins is popn.state.class_mins; min size is the value in the first bin and max size is the value in the last bin
+  // from Adam Langley, sent on 2022-02-22
+  for (int age_ndx = 0;  age_ndx < n_ages; ++age_ndx) {
+    if (ages[age_ndx] > min_x_val_) {
+      // see also https://github.com/nmfs-stock-synthesis/stock-synthesis/blob/main/SS_selex.tpl, lines 436 and 504
+      Type x_p1trans   = ages[age_ndx] - p1trans_;
+      Type x_p2trans   = ages[age_ndx] - p2trans_;
+      Type asc         = exp(-1.0 * (x_p1trans * x_p1trans / p3trans_));
+      Type asc_scaled  = (p5trans_ + (1.0 - p5trans_) * (asc - 0.0) / (1.0 - 0.0));
+      Type desc        = exp(-1.0 * (x_p2trans * x_p2trans / p4trans_));
+      Type stj         = exp(-1.0 * ((40.0 - p2trans_) * (40.0 - p2trans_) / p4trans_));
+      Type desc_scaled = (1.0 + (p6trans_ - 1.0) * (desc - 1.0) /(stj - 1.0));
+      Type join1       = 1.0 / (1.0 + exp(-1.0 * (20.0 * x_p1trans / (1.0 + fabs(x_p1trans)))));
+      Type join2       = 1.0 / (1.0 + exp(-1.0 * (20.0 * x_p2trans / (1.0 + fabs(x_p2trans)))));
+      d_norm[age_ndx] = asc_scaled * (1.0 - join1) + join1 * ((1.0 - join2) + (desc_scaled * join2));
+    } else {
+      d_norm[age_ndx] = exp(first_sel_val_);
+    }
+  }
+  return d_norm;
+}
+
+
+
+
+
+
 /*
  *  BuildSelectivity is the function that will build a selectivity array that can have different time-blocks and a different ogive per block given parameters and control variables
  *  @param sel_params dim: parameters (untransformed) where n_time_blocks is the number of time-blocks n_time_blocks x n_params
@@ -51,6 +87,7 @@ vector<Type> double_normal_ogive(vector<Type>& ages, Type& sel_50, Type& delta) 
  *  3 = Alternative logistic parameterisation
  *  4 = exponential decay selectivity
  *  5 = double normal with 3 parameters
+ *  6 = SS3 double normal with 8 parameters (usually only 3-4 are estimated)
  *  @param ages vector of ages.
  *  @param sel_array is a prepopulated array that will be fulled with the correct . Specifies which time-block selectivity to which year.
  *  @param normalise if true then it will scale the selectivity to have max value = 1
